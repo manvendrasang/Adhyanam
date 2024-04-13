1370b7276f710ac1cbebf7518cdafbba086dd4ba
Manvendra Sang <manvendrasang@gmail.com>
2024-04-13 11:01:19 +0530

    New: minor changes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,7 +9,7 @@ const Main = () => {
             <img className="w-full h-screen object-cover object-left scale-x-[-1]" src="https://images.unsplash.com/photo-1712093828214-0b74bf899528?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
             <div className="w-full h-screen absolute top-0 left-0 bg-black/30">
                 <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
-                    <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">Hello</h1>
+                    <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">Adhyanam: Investing in the tomorrow's leaders, today</h1>
                     <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">We provide students with
                         <TypeAnimation
                             sequence={[
